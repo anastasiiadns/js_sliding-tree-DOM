c9e7e79f255c39f159ea900a600c2b5efe565341
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-10 16:03:07 +0300

    fix of blockers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const collectionLi = document.querySelectorAll('li');
+
+for (const li of collectionLi) {
+  const wrapper = document.createElement('span');
+  const collectionUl = li.querySelector(':scope > ul');
+
+  const arrNode = [];
+
+  li.insertBefore(wrapper, li.firstChild);
+
+  for (const child of li.childNodes) {
+    if (child.nodeType === Node.TEXT_NODE) {
+      arrNode.push(child);
+    }
+  }
+
+  for (const el of arrNode) {
+    wrapper.appendChild(el);
+  }
+
+  wrapper.addEventListener('click', (e) => {
+    if (e.target !== wrapper) {
+      return;
+    }
+
+    if (collectionUl) {
+      collectionUl.style.display =
+        getComputedStyle(collectionUl).display === 'none' ? 'block' : 'none';
+    }
+  });
+}
