57e2545f0eb199040902ce7dce55d01efb15781a
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-10 15:34:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const collectionLi = document.querySelectorAll('li');
+
+for (const li of collectionLi) {
+  const wrapper = document.createElement('span');
+  const collectionUl = li.querySelector('ul');
+
+  const arrNode = [];
+
+  li.insertBefore(wrapper, li.firstChild);
+
+  for (const child of li.childNodes) {
+    if (child.nodeType === Node.TEXT_NODE) {
+      arrNode.push(child);
+    }
+  }
+
+  for (const el of arrNode) {
+    wrapper.appendChild(el);
+  }
+
+  wrapper.addEventListener('click', (e) => {
+    if (collectionUl) {
+      collectionUl.style.display =
+        getComputedStyle(collectionUl).display === 'none' ? 'block' : 'none';
+    }
+  });
+}
